09729d98fb73d7a152d4c57c85f6815863faab8f
Gustavo <gmcotta34@gmail.com>
2021-06-10 21:36:07 -0300

    feat: listen to more stripe events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -19,7 +19,11 @@ export const config = {
   },
 }
 
-const relevantEvents =new Set(['checkout.session.completed']);
+const relevantEvents =new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -39,10 +43,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         switch(type) {
           case 'checkout.session.completed':
             const checkoutSession = event.data.object as Stripe.Checkout.Session; 
+            
             await saveSubscription(
               checkoutSession.subscription.toString(), 
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
+            
+            break;
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+            
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+              
             break;
           default:
             throw new Error('Unhandled event.');
