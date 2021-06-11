9784f41913253026b5a341c39ac4a377f20c25e1
Gustavo <gmcotta34@gmail.com>
2021-06-11 19:23:56 -0300

    feat: listing posts from prismicCMS

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,23 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -14,31 +26,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>10 de junho de 2021</time>
-            <strong>The Plan for React 18</strong>
-            <p>The React team is excited to share a few updates: We’ve started work on the React 18 release...</p>
-          </a>
-          <a href="#">
-            <time>10 de junho de 2021</time>
-            <strong>The Plan for React 18</strong>
-            <p>The React team is excited to share a few updates: We’ve started work on the React 18 release...</p>
-          </a>
-          <a href="#">
-            <time>10 de junho de 2021</time>
-            <strong>The Plan for React 18</strong>
-            <p>The React team is excited to share a few updates: We’ve started work on the React 18 release...</p>
-          </a>
-          <a href="#">
-            <time>10 de junho de 2021</time>
-            <strong>The Plan for React 18</strong>
-            <p>The React team is excited to share a few updates: We’ve started work on the React 18 release...</p>
-          </a>
-          <a href="#">
-            <time>10 de junho de 2021</time>
-            <strong>The Plan for React 18</strong>
-            <p>The React team is excited to share a few updates: We’ve started work on the React 18 release...</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -57,7 +51,23 @@ export const getStaticProps: GetStaticProps = async () => {
 
   console.log(JSON.stringify(response, null, 2));
 
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content
+        .find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    }
+  });
+
   return {
-    props: {}
+    props: { posts }
   }
 }
